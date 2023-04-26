e3af255c9c7a69c7770c9a9584dd8b96624f7c18
Milton Ospina <icmilton.ospina@americanwell.com>
2023-04-26 16:08:33 -0500

    install React Router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import "./App.css";
 import React, { useState } from "react";
 import MovieListPage from "./pages/MovieListPage";
 import MovieContext from "./context";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   const [selectedMovie, setSelectedMovie] = useState(null);
 
   return (
       <MovieContext.Provider value={{ selectedMovie, setSelectedMovie }}>
-        <MovieListPage/>
+        <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MovieListPage/>}/>
+        </Routes>
+        </BrowserRouter>
       </MovieContext.Provider>
   );
 }
